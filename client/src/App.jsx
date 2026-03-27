5da2075455d3bf3e4cbd3dffe1a9193b5f94a381
mdanees055 <aneesalam055@gmail.com>
2026-03-27 12:14:54 +0530

    added some css code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from "./pages/Home"
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookingss from './pages/MyBookingss'
+import Favorite from './pages/Favorite'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
 
 const App = () => {
+
+  const isAdminRoute= useLocation().pathname.startsWith('/admin')
   return (
-    <div>
-      
-    </div>
+    <>
+    <Toaster/>
+      {!isAdminRoute && <Navbar/>}
+
+      <Routes>
+        <Route path='/' elemenet ={<Home/>} />
+        <Route path='/Movies' elemenet ={<Movies/>} />
+        <Route path='/movies/:id' elemenet ={<MovieDetails/>} />
+        <Route path='/movies/:id/:date' elemenet ={<SeatLayout/>} />
+        <Route path='/my-bookings' elemenet ={<MyBookingss/>} />
+        <Route path='/favorite' elemenet ={<Favorite/>} />
+      </Routes>
+      {!isAdminRoute && <Footer/>}
+    </>
   )
 }
 
