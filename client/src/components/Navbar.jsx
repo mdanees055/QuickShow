7e7060d53bf1e3628e6b30e011449bc6e32b14e4
mdanees055 <aneesalam055@gmail.com>
2026-03-30 00:57:30 +0530

    updated login

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import {MenuIcon, SearchIcon, XIcon} from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 
 const Navbar = () => {
 
       const [isOpen, setIsOpen] = useState(false)
+      const{user}=useUser()
+      const{openSignIn}=useClerk()
 
 
 
@@ -16,10 +19,7 @@ const Navbar = () => {
       <img src={assets.logo} alt="" className='w-36 h-auto'/>
       </Link>
 
-      <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg
-      z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen
-      min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20
-      overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
+      <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
 
         <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={()=>setIsOpen(!isOpen)}/>
 
@@ -31,7 +31,14 @@ const Navbar = () => {
 
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>login</button>
+        {
+          !user ? (
+          <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>login</button>
+        ) : ( 
+          <UserButton/> 
+        )
+        }
+        
       </div>
 
           <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' onClick={()=>setIsOpen(!isOpen)}/>
