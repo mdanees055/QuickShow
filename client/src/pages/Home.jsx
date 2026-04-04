2a4869c99cc201fdc1782a3d5e2fbb9c36e96002
mdanees055 <aneesalam055@gmail.com>
2026-04-04 15:00:00 +0530

    updated footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
+import TrailersSection from '../components/trailersSection'
+
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <FeaturedSection/>
+      <TrailersSection/>
+      
     </div>
   )
 }
